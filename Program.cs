af17bd8188d84f1cdd26ca1370b82fd3a61ffe40
poupakkg <60771085+poupakkg@users.noreply.github.com>
2020-05-11 02:00:16 -0600

    adding attemp for the user

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+//using System.IO;
+
 
 
 namespace c__Assignment
@@ -11,8 +13,32 @@ namespace c__Assignment
             Console.WriteLine("Please enter your filename:");
             // get user input and store in local variable
              string fileName = Console.ReadLine();
-            // change user input to blank space
-            //public string Replace(string fileName, string blankSpace);
+
+             //option 2
+             // if file is not valid show error message and return user to main menu - Techie Delight
+            /*
+            bool fileNameExist = FileName.Exists()
+            if (fileNameExist)
+            {
+                Console.WriteLine ( "filename exists" );
+            } 
+            else
+            {
+                Console.WriteLine ( " the filename is not exist, please try again ");
+                 // change user input to blank space or take user to option 1
+            */
+
+            int i = 0;
+            while (i < 4)
+            {
+                Console.WriteLine( "Please enter your filename: " )
+                string rawInput = Console.ReadLine();
+                string stringInput;
+                if (!string.TryParse(rawInput, out stringInput))
+            }
+            }
+           
+            
 
 
         }
