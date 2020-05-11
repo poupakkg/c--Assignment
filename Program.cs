b93aa28929a9a8b5a48d0076d2ff94830512f9ad
poupakkg <60771085+poupakkg@users.noreply.github.com>
2020-05-10 23:01:51 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace c__Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //option 1
+            Console.WriteLine("Please enter your filename:");
+            // get user input and store in local variable
+             string fileName = Console.ReadLine();
+            // change user input to blank space
+            //public string Replace(string fileName, string blankSpace);
+
+
+        }
+    }
+}
