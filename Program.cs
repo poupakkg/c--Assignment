f00788af7b0873ea8f9450d5ef8841ced23c3894
poupakkg <60771085+poupakkg@users.noreply.github.com>
2020-05-12 23:43:46 -0600

    fix the error in switch part by adding bracket

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace c__Assignment
 {
@@ -20,11 +21,11 @@ namespace c__Assignment
             while (!exitRequest){
                 Console.WriteLine("Make a choice!");
                 string makeChoice = Console.ReadLine();
-           
+            }
             switch(option){                            
             //option 1
             case 1:
-            Console.WriteLine("Enter a filename:");
+            Console.WriteLine("Please Enter a filename: ");
             // get user input and store in local variable
             fileName = Console.ReadLine();
             // bool fileNameExist = FileNames.Exists();
@@ -35,16 +36,31 @@ namespace c__Assignment
              break;
             //option 2
             case 2:
-
-             //option 3
+            Console.WriteLine("Please enter a string: ");
+            string stringToReplace = Console.ReadLine();
+            if (File.Exists(fileName))
+            {
+                string readText = File.ReadAllText("fileName");  // Read the contents of the file
+               // Console.WriteLine(readText);  // Output the content
+                string[] splitText = readText.Split( stringToReplace );
+            
+            foreach (string element in splitText){
+                Console.WriteLine(element); //print out our sorted array of names
+            }
+            break;
+            }
+            //option 3
+             case 3:
              exitRequest = true;
              Console.WriteLine("Exit");
              break;
 
-
-
             }
         }
+    }
+}
+
+    
             
 
              // if file is not valid show error message and return user to main menu - Techie Delight
